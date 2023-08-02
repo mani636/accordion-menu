2351250df7bffff024fae34aa95cf2dd3d5c5122
mani636 <manikandanraj817@gmail.com>
2023-08-02 17:21:31 +0530

    accordion menu completed

---
+++ src/components/Question.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+const Question = ({ question, answer }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <article>
+      <header>
+        <h4>{question}</h4>
+        <button className='btn' onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{answer}</p>}
+    </article>
+  );
+};
+
+export default Question;
