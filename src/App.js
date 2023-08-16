24d4c15510d10f87a5f987cc1fbcb4573652fa06
mani636 <manikandanraj817@gmail.com>
2023-08-16 10:38:05 +0530

    remove usestate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import data from './data';
 import Question from './components/Question';
-import { useState } from 'react';
 
 const App = () => {
-  const [questions, setQuestion] = useState(data);
   return (
     <main>
       <div className='container'>
         <h3>Frequently asked Interview questions</h3>
         <section className='info'>
-          {questions.map((question) => {
+          {data.map((question) => {
             return <Question key={question.id} {...question} />;
           })}
         </section>
